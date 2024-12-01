f235a6ac07c51f0163bcf01f9bacf1ce23bd07b1
MohamedMesto <mohamedmesto111@gmail.com>
2024-12-01 21:16:58 +0100

    create Aboutus app

--- loqta2050_prj/urls.py
+++ loqta2050_prj/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from blog2050 import views as blog2050_views
-from home import views as home_views
+from blog2050.views import blog2050_func
+from home.views import home_func
+from aboutus.views import aboutus_func
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog2050/', blog2050_views.blog2050_func, name='blog2050'),
-    path('', home_views.home_func, name='homeviews'),
+    path('', home_func, name='home_func'),
+    path('blog2050/', blog2050_func, name='blog2050_func'),
+    path('aboutus', aboutus_func, name='aboutus_func'),
+
 
 
 
