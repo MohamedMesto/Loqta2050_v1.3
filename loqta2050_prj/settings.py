4d7e265a73341393e7195df93f985a1c7f513209
MohamedMesto <mohamedmesto111@gmail.com>
2024-12-04 22:56:07 +0100

    rewrite s_key at setting

--- loqta2050_prj/settings.py
+++ loqta2050_prj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '@fct(zyj9s_r23eosf!7pviva(bjc71tt7^uny%lizcx$2--zd'
+SECRET_KEY = '@fct(zyj9s_r23eosf!7pviva(bjc71tt7^uny%lizcx$2--zd'
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 
