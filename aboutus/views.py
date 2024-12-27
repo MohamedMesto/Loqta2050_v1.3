847d81f245cebfa4bf2547b1207626ece5a052db
MohamedMesto <mohamedmesto111@gmail.com>
2024-12-27 05:14:32 +0100

    Create the Collaboration Request form

--- aboutus/views.py
+++ aboutus/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Aboutus
+from .forms import CollaborateForm
 
 
 def about_us(request):
@@ -7,9 +8,14 @@ def about_us(request):
     Renders the Aboutus page
     """
     aboutus = Aboutus.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
 
     return render(
         request,
         "aboutus/aboutus.html",
-        {"aboutus": aboutus},
+        {
+            "aboutus": aboutus,
+            "collaborate_form": collaborate_form
+        },
     )
